75449bdb95c2f4e59ab7b7078464ab67066f6330
unknown <mikeliu8492@gmail.com>
2018-05-18 14:50:11 -0500

    mike

--- client/src/views/Redirect.js
+++ client/src/views/Redirect.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import droids from '../images/common/droids.gif'
 
 const Redirect = () => {
     return(<div>
         <h1>404 Error</h1>
         <br/>
         <div style={{display: "flex", justifyContent: "center"}}>
-            <img src={droids} alt="not the page you are looking for"/>
+            <img src="https://drive.google.com/uc?id=1aqctg5oxXNxPdQRr350T1pJuNpy5teRj" alt="not the page you are looking for"/>
         </div>
         <br/>
         <br/>
