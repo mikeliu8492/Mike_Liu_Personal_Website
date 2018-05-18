b4bb773bb41e9091b904518060470797216c2e07
unknown <mikeliu8492@gmail.com>
2018-05-17 22:16:03 -0500

    mike

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require('express'),
     mongoose = require('mongoose'),
     path = require('path'),
     bluebird = require('bluebird')
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser')
+    favicon = require('serve-favicon');
 
 mongoose.Promise = bluebird
 
@@ -20,43 +21,23 @@ const app = express();
 // Connect to a MongoDB
 mongoose.connect(process.env.DB_STRING, { useMongoClient: true});
 
-// Allow CORS so that backend and frontend could be put on different servers
-
-const allowCrossDomain = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-    next();
-};
-app.use(allowCrossDomain);
-
-
 // Use the body-parser package in our application
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")))
 
 // Use api routes as a module as a module (see api.js)
 const api = require('./api.js');
 app.use('/api', api(app, router));
 
-/*
 // Serve up the front-end static JS Bundle contents
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+if(process.env.NODE_ENV !== "production"){
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-*/
-
-if(process.env.NODE_ENV === "production"){
-    console.log("I am in a production environment!!!!!")
-    const my_path = (path.join(__dirname, 'build'))
-    app.use(express.static(my_path));
-
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(my_path, 'index.html'));
+    app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
 else{
