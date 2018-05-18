b313ad996c7745651ebd93558a12bf332b31a7f7
unknown <mikeliu8492@gmail.com>
2018-05-17 22:42:57 -0500

    mike

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express();
 
 const allowCrossDomain = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://mikeliu8492.herokuapp.com/ask_me_anything");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
