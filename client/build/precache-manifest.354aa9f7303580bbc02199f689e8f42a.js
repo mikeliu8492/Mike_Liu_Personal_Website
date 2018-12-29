62936e422fef309e79f53419d5074923775eb125
Mike Liu <mikeliu8492@gmail.com>
2018-12-29 15:41:22 -0600

    added epic to production

---
+++ client/build/precache-manifest.354aa9f7303580bbc02199f689e8f42a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "541a32d1475114f7c864fe02aaa75bb5",
+    "url": "/static/media/ILLINI.541a32d1.jpg"
+  },
+  {
+    "revision": "76edbae2ad03958cbb3e",
+    "url": "/static/css/main.0b34f6fa.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "98abc75315d088c252a4",
+    "url": "/static/js/1.98abc753.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0c6578008a4aaa5cb0895f770a27ae73",
+    "url": "/static/media/mern.0c657800.png"
+  },
+  {
+    "revision": "e83ce987004bfab30752a50465c8fc63",
+    "url": "/static/media/bootstrap.e83ce987.png"
+  },
+  {
+    "revision": "a514c2b26a314bc38c29df6a35ea4ac0",
+    "url": "/static/media/interview.a514c2b2.jpg"
+  },
+  {
+    "revision": "76edbae2ad03958cbb3e",
+    "url": "/static/js/main.76edbae2.chunk.js"
+  },
+  {
+    "revision": "c92b85a5b907c70211f4ec25e29a8c4a",
+    "url": "/static/media/favicon.c92b85a5.ico"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "98abc75315d088c252a4",
+    "url": "/static/css/1.af2c72ee.chunk.css"
+  },
+  {
+    "revision": "f74edee23ffbfe9371e99682bddc6a43",
+    "url": "/index.html"
+  }
+];
